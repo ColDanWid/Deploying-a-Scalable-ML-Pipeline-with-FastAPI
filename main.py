f5c3be2d624fc033b85504ae90ab4f48097c41d1
ColDanWid <cwidmar123@gmail.com>
2025-03-30 10:48:40 -0500

    Update main.py

--- main.py
+++ main.py
@@ -74,20 +74,20 @@ async def greetings():
 @app.post("/inference/")
 async def ingest_data(inference: InputData):
     data = {  'age': inference.age,
-                'workclass': inference.workclass, 
-                'fnlgt': inference.fnlgt,
-                'education': inference.education,
-                'education-num': inference.education_num,
-                'marital-status': inference.marital_status,
-                'occupation': inference.occupation,
-                'relationship': inference.relationship,
-                'race': inference.race,
-                'sex': inference.sex,
-                'capital-gain': inference.capital_gain,
-                'capital-loss': inference.capital_loss,
-                'hours-per-week': inference.hours_per_week,
-                'native-country': inference.native_country,
-                }
+              'workclass': inference.workclass, 
+              'fnlgt': inference.fnlgt,
+              'education': inference.education,
+              'education-num': inference.education_num,
+              'marital-status': inference.marital_status,
+              'occupation': inference.occupation,
+              'relationship': inference.relationship,
+              'race': inference.race,
+              'sex': inference.sex,
+              'capital-gain': inference.capital_gain,
+              'capital-loss': inference.capital_loss,
+              'hours-per-week': inference.hours_per_week,
+              'native-country': inference.native_country,
+            }
 
     # prepare the sample for inference as a dataframe
     sample = pd.DataFrame(data, index=[0])
@@ -116,17 +116,18 @@ async def ingest_data(inference: InputData):
                                 lb=lb
                                 )
 
-    # get model prediction which is a one-dim array like [1]                            
+    # get model prediction which is a one-dim array like [1]                             
     prediction = model.predict(sample)
 
     # convert prediction to label and add to data output
     if prediction[0] > 0.5:
         prediction = '>50K'
     else:
-        prediction = '<=50K', 
+        prediction = '<=50K'
     data['prediction'] = prediction
 
     return data
 
 if __name__ == '__main__':
     pass
+
