de334c91dee515de6d928c8df469a2a628f83d29
ColDanWid <cwidmar123@gmail.com>
2025-03-29 21:12:03 -0500

    Setup main

--- main.py
+++ main.py
@@ -1,74 +1,136 @@
-import os
-
+# Put the code for your API here.
+from fastapi import FastAPI, HTTPException
+from typing import Union, Optional
+# BaseModel from Pydantic is used to define data objects
+from pydantic import BaseModel
 import pandas as pd
-from fastapi import FastAPI
-from pydantic import BaseModel, Field
+import os, pickle
+from ml.data import process_data
 
-from ml.data import apply_label, process_data
-from ml.model import inference, load_model
+ # path to saved artifacts
+savepath = './model'
+filename = ['trained_model.pkl', 'encoder.pkl', 'labelizer.pkl']
 
-# DO NOT MODIFY
-class Data(BaseModel):
-    age: int = Field(..., example=37)
-    workclass: str = Field(..., example="Private")
-    fnlgt: int = Field(..., example=178356)
-    education: str = Field(..., example="HS-grad")
-    education_num: int = Field(..., example=10, alias="education-num")
-    marital_status: str = Field(
-        ..., example="Married-civ-spouse", alias="marital-status"
-    )
-    occupation: str = Field(..., example="Prof-specialty")
-    relationship: str = Field(..., example="Husband")
-    race: str = Field(..., example="White")
-    sex: str = Field(..., example="Male")
-    capital_gain: int = Field(..., example=0, alias="capital-gain")
-    capital_loss: int = Field(..., example=0, alias="capital-loss")
-    hours_per_week: int = Field(..., example=40, alias="hours-per-week")
-    native_country: str = Field(..., example="United-States", alias="native-country")
+# Declare the data object with its components and their type.
+class InputData(BaseModel):
+    age: int
+    workclass: str 
+    fnlgt: int
+    education: str
+    education_num: int
+    marital_status: str
+    occupation: str
+    relationship: str
+    race: str
+    sex: str
+    capital_gain: int
+    capital_loss: int
+    hours_per_week: int
+    native_country: str
 
-path = None # TODO: enter the path for the saved encoder 
-encoder = load_model(path)
+    class Config:
+        schema_extra = {
+                        "example": {
+                                    'age':50,
+                                    'workclass':"Private", 
+                                    'fnlgt':234721,
+                                    'education':"Doctorate",
+                                    'education_num':16,
+                                    'marital_status':"Separated",
+                                    'occupation':"Exec-managerial",
+                                    'relationship':"Not-in-family",
+                                    'race':"Black",
+                                    'sex':"Female",
+                                    'capital_gain':0,
+                                    'capital_loss':0,
+                                    'hours_per_week':50,
+                                    'native_country':"United-States"
+                                    }
+                        }
 
-path = None # TODO: enter the path for the saved model 
-model = load_model(path)
 
-# TODO: create a RESTful API using FastAPI
-app = None # your code here
+# instantiate FastAPI app
+app = FastAPI(  title="Inference API",
+                description="An API that takes a sample and runs an inference",
+                version="1.0.0")
 
-# TODO: create a GET on the root giving a welcome message
+# load model artifacts on startup of the application to reduce latency
+@app.on_event("startup")
+async def startup_event(): 
+    global model, encoder, lb
+    # if saved model exits, load the model from disk
+    if os.path.isfile(os.path.join(savepath,filename[0])):
+        model = pickle.load(open(os.path.join(savepath,filename[0]), "rb"))
+        encoder = pickle.load(open(os.path.join(savepath,filename[1]), "rb"))
+        lb = pickle.load(open(os.path.join(savepath,filename[2]), "rb"))
+
+
 @app.get("/")
-async def get_root():
-    """ Say hello!"""
-    # your code here
-    pass
+async def greetings():
+    return "Welcome to our model API"
 
 
-# TODO: create a POST on a different path that does model inference
-@app.post("/data/")
-async def post_inference(data: Data):
-    # DO NOT MODIFY: turn the Pydantic model into a dict.
-    data_dict = data.dict()
-    # DO NOT MODIFY: clean up the dict to turn it into a Pandas DataFrame.
-    # The data has names with hyphens and Python does not allow those as variable names.
-    # Here it uses the functionality of FastAPI/Pydantic/etc to deal with this.
-    data = {k.replace("_", "-"): [v] for k, v in data_dict.items()}
-    data = pd.DataFrame.from_dict(data)
+# This allows sending of data (our InferenceSample) via POST to the API.
+@app.post("/inference/")
+async def ingest_data(inference: InputData):
+    data = {  'age': inference.age,
+                'workclass': inference.workclass, 
+                'fnlgt': inference.fnlgt,
+                'education': inference.education,
+                'education-num': inference.education_num,
+                'marital-status': inference.marital_status,
+                'occupation': inference.occupation,
+                'relationship': inference.relationship,
+                'race': inference.race,
+                'sex': inference.sex,
+                'capital-gain': inference.capital_gain,
+                'capital-loss': inference.capital_loss,
+                'hours-per-week': inference.hours_per_week,
+                'native-country': inference.native_country,
+                }
 
+    # prepare the sample for inference as a dataframe
+    sample = pd.DataFrame(data, index=[0])
+
+    # apply transformation to sample data
     cat_features = [
-        "workclass",
-        "education",
-        "marital-status",
-        "occupation",
-        "relationship",
-        "race",
-        "sex",
-        "native-country",
-    ]
-    data_processed, _, _, _ = process_data(
-        # your code here
-        # use data as data input
-        # use training = False
-        # do not need to pass lb as input
-    )
-    _inference = None # your code here to predict the result using data_processed
-    return {"result": apply_label(_inference)}
+                    "workclass",
+                    "education",
+                    "marital-status",
+                    "occupation",
+                    "relationship",
+                    "race",
+                    "sex",
+                    "native-country",
+                    ]
+
+    # if saved model exits, load the model from disk
+    if os.path.isfile(os.path.join(savepath,filename[0])):
+        model = pickle.load(open(os.path.join(savepath,filename[0]), "rb"))
+        encoder = pickle.load(open(os.path.join(savepath,filename[1]), "rb"))
+        lb = pickle.load(open(os.path.join(savepath,filename[2]), "rb"))
+        
+    sample,_,_,_ = process_data(
+                                sample, 
+                                categorical_features=cat_features, 
+                                training=False, 
+                                encoder=encoder, 
+                                lb=lb
+                                )
+
+    # get model prediction which is a one-dim array like [1]                            
+    prediction = model.predict(sample)
+
+    # convert prediction to label and add to data output
+    if prediction[0]>0.5:
+        prediction = '>50K'
+    else:
+        prediction = '<=50K', 
+    data['prediction'] = prediction
+
+
+    return data
+
+
+if __name__ == '__main__':
+    pass
